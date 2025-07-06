0906b289c8bd001ec773d8bfc45084b42a7f1ce4
Shivam Singh <rajputshiv1610@gmail.com>
2025-07-06 16:22:31 +0530

    basic-setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import ChatList from "./components/ChatList"
+import ChatWindow from "./components/ChatWindow"
+
+function App() {
+
+  return (
+    <>
+    <div>Chat App</div>
+    <div>
+      <ChatList />
+      <ChatWindow />
+    </div>
+    </>
+    
+  )
+}
+
+export default App
